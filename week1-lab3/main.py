e0b115e94f3eb0e10d34c1b0e83b3ef898a5e4e6
Bharat kumar <bharatkn786@gmail.com>
2026-08-08 17:32:31 +0530

    Task 2->>using counter

--- week1-lab3/main.py
+++ week1-lab3/main.py
@@ -23,6 +23,33 @@ def word_count(text):
     # print(dict)
     return dict
 
+
+
+# Task2---->Using counter
+
+def word_count_counter(text):
+    text=text.lower()
+
+    clean=""
+
+    for ch in text:
+        if ch.isdigit() or ch.isalpha() or ch==" ":
+            clean+=ch
+    clean=clean.split()     
+
+    # print(dict(Counter(clean)))
+    return dict(Counter(clean))
+
+
+
+
+
+
 if __name__ == "__main__":
     text="Hello, hello World! 123"
     print(word_count(text))
+
+    print(word_count_counter(text))
+
+    print("\n Do both methods give same output? ")
+    print(word_count(text)==word_count_counter(text))
